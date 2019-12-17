2b713e58f2d408d82cfe46c0377c327560468194
jeonghoj <jeonghoj94@gmail.com>
2019-12-18 01:17:47 +0900

    테스트 코드 임시 삭제

--- src/test/java/jeonghoj/boardproject/BoardProjectApplicationTests.java
+++ src/test/java/jeonghoj/boardproject/BoardProjectApplicationTests.java
@@ -1,24 +1,24 @@
-package jeonghoj.boardproject;
-
-import jeonghoj.boardproject.repository.GeneralBoardRepository;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class BoardProjectApplicationTests {
-
-	@Autowired
-	private GeneralBoardRepository generalBoardRepository;
-
-	@Test
-	public void closedProjections() {
-
-
-
-	}
-
-}
+//package jeonghoj.boardproject;
+//
+//import jeonghoj.boardproject.repository.GeneralBoardRepository;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class BoardProjectApplicationTests {
+//
+//	@Autowired
+//	private GeneralBoardRepository generalBoardRepository;
+//
+//	@Test
+//	public void closedProjections() {
+//
+//
+//
+//	}
+//
+//}
